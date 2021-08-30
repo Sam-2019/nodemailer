a2aef2c09ced3ce8c0de814e20b78c3bdd485022
Sam-2019 <samtmartey@gmail.com>
2021-08-30 11:12:25 +0100

    nodemailer

---
+++ index.js
@@ -0,0 +1,53 @@
+"use strict";
+const dotenv = require("dotenv");
+const { google } = require("googleapis");
+const nodemailer = require("nodemailer");
+
+dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+async function sendMail() {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "your authorised email address",
+        clientId: CLIENT_ID,
+        clientSecret: CLEINT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
+
+    const mailOptions = {
+      from: "SENDER NAME <yours authorised email address@gmail.com>",
+      to: "recepient's address here",
+      subject: "Hello gmail using API",
+      text: "Hello gmail using API",
+      html: "<h1>Hello gmail using API</h1>",
+    };
+
+    const result = await transport.sendMail(mailOptions);
+    return result;
+  } catch (error) {
+    return error;
+  }
+}
+
+sendMail()
+  .then((result) => console.log("Email sent....", result))
+  .catch((err) => console.log(err));
